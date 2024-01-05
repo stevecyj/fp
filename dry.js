117104c32c9aab6a6a5e8fbe2d85e92d855edd97
steve.tsao <steve.tsao@hlwtech.com.tw>
2024-01-05 10:51:49 +0800

    refactor: refactor computation logic and add new functions
    - Comment out the `map` function and its usage
    - Add `add1`, `mult3`, and `divide2` functions
    - Modify `arrCompute` function to use `arr.length` instead of `len`
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- dry.js
+++ dry.js
@@ -1,11 +1,37 @@
-const map = (arr, cb) => {
-  const len = arr.length
+// const map = (arr, cb) => {
+//   const len = arr.length
+//   const newArr = []
+//   for (let i = 0; i < len; i++) {
+//     newArr.push(cb(arr[i]))
+//   }
+//   return newArr
+// }
+
+// const mapResult = map([1, 2, 3], (item) => item * 2)
+// console.log(mapResult)
+
+function add1(num) {
+  return num + 1
+}
+
+function mult3(num) {
+  return num * 3
+}
+
+function divide2(num) {
+  return num / 2
+}
+
+const arr = [1, 2, 3]
+
+function arrCompute(arr, compute) {
   const newArr = []
-  for (let i = 0; i < len; i++) {
-    newArr.push(cb(arr[i]))
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(compute(arr[i]))
   }
   return newArr
 }
 
-const mapResult = map([1, 2, 3], (item) => item * 2)
-console.log(mapResult)
+console.log(arrCompute(arr, add1))
+console.log(arrCompute(arr, mult3))
+console.log(arrCompute(arr, divide2))
