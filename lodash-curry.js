ba08f7b729f926d7b25e0dd59898fb1be7433f48
steve.tsao <steve.tsao@hlwtech.com.tw>
2024-01-05 16:22:55 +0800

    feat: add lodash-curry.js and getChildren function for working with child nodes
    - Add file `lodash-curry.js`
    - Define function `getChildren` that returns the child nodes of a given element
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

---
+++ lodash-curry.js
@@ -0,0 +1,10 @@
+// let curry = require("lodash").curry
+// let match = curry(function (what, str) {
+//   return str.match(what)
+// })
+
+// console.log(match(/\s+/g, "hallo world"))
+
+var getChildren = function (x) {
+  return x.childNodes
+}
